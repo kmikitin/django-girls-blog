5aa3ddce64f8907315d7211aff5f41aff448f0dc
Kait Mikitin <kait.mikitin@gmail.com>
2018-03-03 16:37:01 -0600

    added security auth to login/logout

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
-# this is how you instantiate a class in Django
+
 class Post(models.Model):
-	# foreignkey is part of the relational side of SQLite
-	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-	title = models.CharField(max_length=200)
-	text = models.TextField()
-	created_date = models.DateTimeField(
-		default=timezone.now)
-	published_date = models.DateTimeField(
-		blank=True, null=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
 
-	def publish(self):
-		self.publish_date = timezone.now()
-		self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
